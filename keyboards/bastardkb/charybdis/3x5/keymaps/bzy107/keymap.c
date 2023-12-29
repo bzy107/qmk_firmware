6387352b95c310fad88454c8f0ea48b1c85edc70
bzy107 <bzy107@gmail.com>
2023-12-29 16:34:32 +0900

    charybdisのキーマップ修正

--- keyboards/bastardkb/charybdis/3x5/keymaps/bzy107/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/bzy107/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      // ├─────────────────────────────────────────────┤                         ├─────────────────────────────────────────────┤
                KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
      // ├─────────────────────────────────────────────┤                         ├─────────────────────────────────────────────┤
-          MT(MOD_LCTL, KC_Z), MT(MOD_LCTL, KC_X),  KC_C,    KC_V,    KC_B,         KC_N,    KC_M, KC_COMM,  KC_DOT, MT(ONE, KC_SLSH),
+          MT(MOD_LCTL, KC_Z), MT(MOD_LCTL, KC_X),  KC_C,    KC_V,    KC_B,         KC_N, KC_M, KC_COMM, KC_DOT, LT(ONE, KC_SLSH),
      // ╰─────────────────────────────────────────────┤                         ├─────────────────────────────────────────────╯
                               TWO, KC_SPC,   KC_LSFT,                              KC_BSPC, MT(MOD_LGUI, KC_ENT)
      //                   ╰───────────────────────────╯                         ╰──────────────────╯
