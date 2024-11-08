ae8251336799e1316232df50f09da30361844c7d
bzy107 <bzy107@gmail.com>
2024-11-09 03:37:48 +0900

    wip

--- keyboards/taida/config.h
+++ keyboards/taida/config.h
@@ -1,22 +1,108 @@
-// Copyright 2024 bzy107 (@bzy107)
-// SPDX-License-Identifier: GPL-2.0-or-later
+// // Copyright 2024 bzy107 (@bzy107)
+// // SPDX-License-Identifier: GPL-2.0-or-later
+
+// #pragma once
+
+// /*
+//  * Feature disable options
+//  *  These options are also useful to firmware size reduction.
+//  */
+
+// /* disable debug print */
+// //#define NO_DEBUG
+
+// /* disable print */
+// //#define NO_PRINT
+
+// /* disable action features */
+// //#define NO_ACTION_LAYER
+// //#define NO_ACTION_TAPPING
+// //#define NO_ACTION_ONESHOT
+// #define SOFT_SERIAL_PIN D2
+// #define MASTER_LEFT
+
+
+// #pragma once
+
+// #ifdef RGBLIGHT_ENABLE
+// #    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #    define RGBLIGHT_EFFECT_SNAKE
+// #    define RGBLIGHT_EFFECT_KNIGHT
+// #    define RGBLIGHT_EFFECT_CHRISTMAS
+// #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #    define RGBLIGHT_EFFECT_RGB_TEST
+// #    define RGBLIGHT_EFFECT_ALTERNATING
+// #    define RGBLIGHT_EFFECT_TWINKLE
+// #endif
+
+// #define TAP_CODE_DELAY 5
+
+// #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+// #define AUTO_MOUSE_DEFAULT_LAYER 1
+
+// #define RGBLED_NUM 6
+
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
+// Key matrix parameters
+#define MATRIX_ROWS         (4 * 2)  // split keyboard
+#define MATRIX_COLS         6
+#define MATRIX_ROW_PINS     { F4, F5, F6, F7 }
+#define MATRIX_COL_PINS     { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_MASKED
+#define DEBOUNCE            5
+#define DIODE_DIRECTION     COL2ROW
 
-/* disable debug print */
-//#define NO_DEBUG
+// Split parameters
+#define SOFT_SERIAL_PIN         D2
+#define SPLIT_HAND_MATRIX_GRID  F6, B5
+#define SPLIT_USB_DETECT
+#ifdef OLED_ENABLE
+#    define SPLIT_OLED_ENABLE
+#endif
 
-/* disable print */
-//#define NO_PRINT
+// If your PC does not recognize Keyball, try setting this macro. This macro
+// increases the firmware size by 200 bytes, so it is disabled by default, but
+// it has been reported to work well in such cases.
+//#define SPLIT_WATCHDOG_ENABLE
 
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-#define SOFT_SERIAL_PIN D2
-#define MASTER_LEFT
+#define SPLIT_TRANSACTION_IDS_KB KEYBALL_GET_INFO, KEYBALL_GET_MOTION, KEYBALL_SET_CPI
+
+// RGB LED settings
+#define WS2812_DI_PIN       D3
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLED_NUM      48
+#    define RGBLED_SPLIT    { 24, 24 }  // (24 + 22)
+#    ifndef RGBLIGHT_LIMIT_VAL
+#        define RGBLIGHT_LIMIT_VAL  150 // limitated for power consumption
+#    endif
+#    ifndef RGBLIGHT_VAL_STEP
+#        define RGBLIGHT_VAL_STEP   15
+#    endif
+#    ifndef RGBLIGHT_HUE_STEP
+#        define RGBLIGHT_HUE_STEP   17
+#    endif
+#    ifndef RGBLIGHT_SAT_STEP
+#        define RGBLIGHT_SAT_STEP   17
+#    endif
+#endif
+#ifdef RGB_MATRIX_ENABLE
+#    define RGB_MATRIX_SPLIT    { 24, 24 }
+#endif
+
+#ifndef OLED_FONT_H
+#    define OLED_FONT_H "lib/logofont.c"
+#    define OLED_FONT_START 32
+#    define OLED_FONT_END 195
+#endif
+
+#if !defined(LAYER_STATE_8BIT) && !defined(LAYER_STATE_16BIT) && !defined(LAYER_STATE_32BIT)
+#    define LAYER_STATE_8BIT
+#endif
+
+// To squeeze firmware size
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
--- keyboards/taida/keymaps/default/keymap.c
+++ keyboards/taida/keymaps/default/keymap.c
@@ -1,4 +1,64 @@
+// #include QMK_KEYBOARD_H
+
+// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//     [0] = LAYOUT(KC_LGUI)
+// };
+
 #include QMK_KEYBOARD_H
+
+// #include "quantum.h"
+
+// // clang-format off
+// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//   // keymap for default
+//   [0] = LAYOUT_universal(
+//     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+//     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
+//     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
+//     KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
+//   ),
+
+//   [1] = LAYOUT_universal(
+//     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
+//     KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                           S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
+//     S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
+//     KC_INT1  , KC_EQL   , S(KC_3)  , _______  , _______  , _______  ,      TO(2)    , TO(0)    , _______  , KC_RALT  , KC_RGUI  , KC_F12
+//   ),
+
+//   [2] = LAYOUT_universal(
+//     KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                            KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
+//    S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
+//     KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                           S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
+//     KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
+//   ),
+
+//   [3] = LAYOUT_universal(
+//     RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
+//     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
+//     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+//     QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
+//   ),
+// };
+// // clang-format on
+
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     // Auto enable scroll mode when the highest layer is 3
+//     keyball_set_scroll_mode(get_highest_layer(state) == 3);
+//     return state;
+// }
+
+// #ifdef OLED_ENABLE
+
+// #    include "lib/oledkit/oledkit.h"
+
+// void oledkit_render_info_user(void) {
+//     keyball_oled_render_keyinfo();
+//     keyball_oled_render_ballinfo();
+//     keyball_oled_render_layerinfo();
+// }
+// #endif
+
+
 
 // Defines the keycodes used by our macros in process_record_user
 #define _ZERO 0
@@ -13,13 +73,13 @@
 #define RESET QK_BOOT
 
 enum custom_keycodes {
-  ZERO = SAFE_RANGE,
-  ONE,
-  TWO,
-  THREE,
-  FOUR,
-  FIVE,
-  SIX
+    ZERO = SAFE_RANGE,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,9 +97,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_ZERO] = LAYOUT(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  MT(MOD_LCTL, KC_Z),    MT(MOD_LCTL, KC_X),    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  MT(MOD_RALT, KC_SLSH),
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+    MT(MOD_LCTL, KC_Z),    MT(MOD_LCTL, KC_X),    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  MT(MOD_RALT, KC_SLSH),
                     TWO,     KC_SPC,  KC_LSFT,      KC_BSPC, MT(MOD_LGUI, KC_ENT), ONE
 ),
 
@@ -56,10 +116,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'             `--------------------'
  */
 [_ONE] = LAYOUT(
-  KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,        KC_PLUS, KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,
-  KC_TAB,  KC_PERC, KC_ASTR, KC_AMPR, KC_QUOT,       KC_EQL , KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,
-  KC_LCTL, KC_GRV , KC_CIRC, KC_BSLS, KC_LSFT,       KC_MINS, KC_F9 ,  KC_F10,  KC_F11,  SIX,
-                      THREE, KC_SPC,  KC_LSFT,       KC_DEL,  MT(MOD_LGUI, KC_ENT), _______
+    KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,        KC_PLUS, KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,
+    KC_TAB,  KC_PERC, KC_ASTR, KC_AMPR, KC_QUOT,       KC_EQL , KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,
+    KC_LCTL, KC_GRV , KC_CIRC, KC_BSLS, KC_LSFT,       KC_MINS, KC_F9 ,  KC_F10,  KC_F11,  SIX,
+                    THREE, KC_SPC,  KC_LSFT,       KC_DEL,  MT(MOD_LGUI, KC_ENT), _______
 ),
 
 /* TWO
@@ -75,9 +135,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'         `--------------------'
  */
 [_TWO] = LAYOUT(
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_QUOT, KC_NO, KC_UP ,   KC_LBRC,  KC_RBRC,
-  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_EQL , KC_LEFT, KC_LPRN,  KC_RIGHT, KC_RPRN,
-  KC_LCTL, KC_TAB,  KC_RALT, KC_LSFT, FIVE,       KC_MINS, KC_NO, KC_DOWN,  KC_LCBR,  KC_RCBR,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_QUOT, KC_NO, KC_UP ,   KC_LBRC,  KC_RBRC,
+    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_EQL , KC_LEFT, KC_LPRN,  KC_RIGHT, KC_RPRN,
+    KC_LCTL, KC_TAB,  KC_RALT, KC_LSFT, FIVE,       KC_MINS, KC_NO, KC_DOWN,  KC_LCBR,  KC_RCBR,
                     _______, KC_SPC,  KC_LSFT,    KC_DEL, MT(MOD_LGUI, KC_ENT), FOUR
 ),
 
@@ -94,9 +154,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_THREE] =  LAYOUT(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_RALT, KC_LCTL, KC_LGUI,
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_RALT, KC_LCTL, KC_LGUI,
                     _______, KC_SPC,  KC_LSFT,      KC_BSPC, KC_ENT, _______
 ),
 
@@ -113,9 +173,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_FOUR] =  LAYOUT(
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                 KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO
 ),
 
@@ -132,64 +192,64 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_FIVE] =  LAYOUT(
-  KC_NO, KC_NO, KC_NO, KC_NO, LCG_NRM,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_NO, KC_NO, KC_NO, KC_NO, LCG_SWP,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  WIN_H, MAC_H, KC_NO, KC_NO, KC_NO,      KC_RALT, KC_TILD, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, LCG_NRM,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, LCG_SWP,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    WIN_H, MAC_H, KC_NO, KC_NO, KC_NO,      KC_RALT, KC_TILD, KC_NO, KC_NO, KC_NO,
                     KC_NO, KC_NO, RESET,      KC_NO,  KC_NO, KC_NO
 )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case ZERO:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_ZERO);
-      }
-      return false;
-      break;
-    case ONE:
-      if (record->event.pressed) {
-        layer_on(_ONE);
-        update_tri_layer(_ONE, _THREE, _SIX);
-      } else {
-        layer_off(_ONE);
-        update_tri_layer(_ONE, _THREE, _SIX);
-      }
-      return false;
-      break;
-    case TWO:
-      if (record->event.pressed) {
-        layer_on(_TWO);
-        update_tri_layer(_TWO, _FOUR, _FIVE);
-      } else {
-        layer_off(_TWO);
-        update_tri_layer(_TWO, _FOUR, _FIVE);
-      }
-      return false;
-      break;
-    case THREE:
-      if (record->event.pressed) {
-        layer_on(_THREE);
-      } else {
-        layer_off(_THREE);
-      }
-      return false;
-      break;
-    case FOUR:
-      if (record->event.pressed) {
-        layer_on(_FOUR);
-      } else {
-        layer_off(_FOUR);
-      }
-      return false;
-      break;
-    case FIVE:
-      if (record->event.pressed) {
-        layer_on(_FIVE);
-      } else {
-        layer_off(_FIVE);
-      }
-      break;
-  }
-  return true;
+    switch (keycode) {
+        case ZERO:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_ZERO);
+            }
+            return false;
+            break;
+        case ONE:
+            if (record->event.pressed) {
+                layer_on(_ONE);
+                update_tri_layer(_ONE, _THREE, _SIX);
+            } else {
+                layer_off(_ONE);
+                update_tri_layer(_ONE, _THREE, _SIX);
+            }
+            return false;
+            break;
+        case TWO:
+            if (record->event.pressed) {
+                layer_on(_TWO);
+                update_tri_layer(_TWO, _FOUR, _FIVE);
+            } else {
+                layer_off(_TWO);
+                update_tri_layer(_TWO, _FOUR, _FIVE);
+            }
+            return false;
+            break;
+        case THREE:
+            if (record->event.pressed) {
+                layer_on(_THREE);
+            } else {
+                layer_off(_THREE);
+            }
+            return false;
+            break;
+        case FOUR:
+            if (record->event.pressed) {
+                layer_on(_FOUR);
+            } else {
+                layer_off(_FOUR);
+            }
+            return false;
+            break;
+        case FIVE:
+            if (record->event.pressed) {
+                layer_on(_FIVE);
+            } else {
+                layer_off(_FIVE);
+            }
+            break;
+    }
+    return true;
 }
